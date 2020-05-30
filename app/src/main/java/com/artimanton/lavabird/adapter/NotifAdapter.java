4f6493cdaf4bbd3e2eabac56cfcf934736a4d474
artim-anton <artim-anton@yandex.ru>
2020-05-30 12:29:12 +0300

    add RecyclerView, Adapter, view_item

---
+++ app/src/main/java/com/artimanton/lavabird/adapter/NotifAdapter.java
@@ -0,0 +1,58 @@
+package com.artimanton.lavabird.adapter;
+
+import android.annotation.SuppressLint;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.artimanton.lavabird.R;
+import com.artimanton.lavabird.NotifEntity;
+
+import java.util.List;
+
+public class NotifAdapter extends RecyclerView.Adapter<NotifAdapter.RecordViewHolder> {
+
+    private List<NotifEntity> list;
+
+    public NotifAdapter(List<NotifEntity> list) {
+        this.list = list;
+    }
+
+    @NonNull
+    @Override
+    public RecordViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new RecordViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.view_item_notif, parent, false));
+    }
+
+    @SuppressLint("ResourceAsColor")
+    @Override
+    public void onBindViewHolder(@NonNull RecordViewHolder holder, int position) {
+        final NotifEntity notifEntity = list.get(position);
+
+        holder.etPack.setText(notifEntity.packages);
+        holder.etText.setText(notifEntity.text);
+    }
+
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+
+
+    class RecordViewHolder extends RecyclerView.ViewHolder{
+        private TextView etPack, etText,  etTime, etData;
+
+        private RecordViewHolder(View itemView) {
+            super(itemView);
+            etPack = itemView.findViewById(R.id.et_pack);
+            etText = itemView.findViewById(R.id.et_text);
+            //etData = itemView.findViewById(R.id.et_date);
+            //etTime = itemView.findViewById(R.id.et_time);
+        }
+    }
+}
